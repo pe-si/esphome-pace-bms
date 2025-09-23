8149af9647fdaea256425d0396be18cf756ae88d
Nick Kinnan <nick@nickkinnan.com>
2025-09-22 21:22:11 -0700

    speculative

--- components/pace_bms/pace_bms_protocol_v25.cpp
+++ components/pace_bms/pace_bms_protocol_v25.cpp
@@ -129,10 +129,10 @@ bool PaceBmsProtocolV25::ProcessReadAnalogInformationResponse(const uint8_t busI
 	{
 		byteOffset += 2;
 	}
-	// reported by johnmsole that Eenovance/Sunsynk packs have an 2 extra temperatures (2 * 4 bytes) plus an extra 4 bytes at the end containing unknown information for a total of 12 extra bytes
+	// reported by johnmsole that Eenovance/Sunsynk packs have an 2 extra temperatures (2 * 4 bytes) plus an extra 28 bytes at the end containing unknown information for a total of 36 extra bytes
 	else if (AnalogInformationUserDefinedValue == 4)
 	{
-		byteOffset += 4; // actually 12, but we skipped past the extra temperatures already
+		byteOffset += 28; 
 	}
 
 	if (byteOffset != payloadLen + 13)
