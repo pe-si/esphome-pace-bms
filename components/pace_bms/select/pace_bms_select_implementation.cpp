21eeb78d20b9b65cb1bb6fc443a1ec913f9b6746
Nick Kinnan <nick@nickkinnan.com>
2025-12-10 22:46:15 -0800

    fix build error due to esphome update

--- components/pace_bms/select/pace_bms_select_implementation.cpp
+++ components/pace_bms/select/pace_bms_select_implementation.cpp
@@ -21,13 +21,13 @@ void PaceBmsSelectImplementation::control(const std::string& text) {
 
 uint8_t PaceBmsSelectImplementation::value_from_option(std::string text)
 {
-	auto options = this->traits.get_options();
-	auto opt_it = std::find(options.cbegin(), options.cend(), text);
-	if (opt_it == options.cend()) {
+	const auto &options = this->traits.get_options();
+	auto opt_it = std::find(options.begin(), options.end(), text);
+	if (opt_it == options.end()) {
 		ESP_LOGD(TAG, "%s: unable to find value for option '%s'", this->name_.c_str(), text.c_str());
 		return 0xFF;
 	}
-	size_t idx = std::distance(options.cbegin(), opt_it);
+	size_t idx = std::distance(options.begin(), opt_it);
 	uint8_t value = this->values_[idx];
 
 	ESP_LOGD(TAG, "%s: found value 0x%02X for option '%s'", this->name_.c_str(), value, text.c_str());
@@ -36,13 +36,13 @@ uint8_t PaceBmsSelectImplementation::value_from_option(std::string text)
 }
 
 std::string PaceBmsSelectImplementation::option_from_value(uint8_t value) {
-	auto options = this->traits.get_options();
-	auto opt_it = std::find(this->values_.cbegin(), this->values_.cend(), value);
-	if (opt_it == this->values_.cend()) {
+	const auto &options = this->traits.get_options();
+	auto opt_it = std::find(this->values_.begin(), this->values_.end(), value);
+	if (opt_it == this->values_.end()) {
 		ESP_LOGD(TAG, "%s: unable to find option for value 0x%02X", this->name_.c_str(), value);
 		return std::string("error");
 	}
-	size_t idx = std::distance(this->values_.cbegin(), opt_it);
+	size_t idx = std::distance(this->values_.begin(), opt_it);
 	std::string text = std::string(options[idx]);
 
 	ESP_LOGD(TAG, "%s: found option '%s' for value 0x%02X", this->name_.c_str(), text.c_str(), value);
